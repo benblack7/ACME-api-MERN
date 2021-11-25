9af8d082b82d0c307c4ccb05f95ba8607e924c20
Ben Black <benblack7@gmail.com>
2021-11-25 13:28:38 -0600

    Removed test cases

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use(bodyParser.json())
 
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-// app.post('/buyers', buyerRouter);
-// app.get('/buyers', buyerRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
